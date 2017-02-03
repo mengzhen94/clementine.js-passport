5a58a3a5f3bce3a13c84fb76dda88ae2ad9de0fb
Mengzhen <gao.mengzhen94@gmail.com>
2017-02-03 16:41:06 -0500

    fix angular for client.js s

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -12,51 +12,35 @@ other variables within the application that may share the same name or need to b
 	angular
 	.module('clementineApp', ['ngResource'])
 	.controller('clickController', 
-		['$scope', '$resource', function($scope, $resource){
+		['$scope', '$http', function($scope, $http){
+
+			$scope.getClicks = function(){
+				$http.get('/api/:id/clicks').then(function(response){
+					$scope.clicks = response.data.clicks;
+				});
+			}
+
+			$scope.getClicks();
+			
+			$http.get("/api/:id").then(function(response){
+				$scope.displayName = response.data.displayName;
+			});
+
+			$scope.addClick = function(){
+				$http.post('/api/:id/clicks').then(function(){
+					$scope.getClicks();
+				});
+			};
+
+			$scope.resetClicks = function(){
+				$http.delete('/api/:id/clicks').then(function(){
+					$scope.getClicks();
+				});
+			}
+
+			
 		
-		//This new $resource object allows us to query this API, 
-		//and will return the results to a field in the browser.
-		var Click = $resource('/api/17734819/clicks');
-
-		//bind the getClicks method to $scope
-		$scope.getClicks = function(){
-			/*
-			 This can then be either manipulated in some way before passing it on to 
-			 he browser, or (as in our case) just pass it straight in to a variable on 
-			 the $scope. 
-			*/
-
-			//Click.get() will make an HTTP GET request to the API and return all of the results.
-			Click.get(function(results){
-				$scope.clicks = results.clicks;
-			});
-		}
-
-		$scope.getClicks();
-
-		/*
-		For addClick, we're instructing Angular to use $resource.save, which will 
-		prompt an HTTP POST request. This in turn will get routed by our index.js 
-		file and run the clickHandler.addClick method on the database. 
-		Once that action has been performed, we query the API via $scope.getClicks, 
-		therefore forcing the $scope.clicks variable to update and represent 
-		the new number of clicks.
-		*/
-
-		//prompt an HTTP POST request
-		$scope.addClick = function(){
-			Click.save(function(){
-				$scope.getClicks();
-			});
-		};
 		
-		//HTTP DELETE request
-		$scope.resetClicks = function(){
-			Click.remove(function(){
-				$scope.getClicks();
-			});
-		};
-
 	}]);
 
 })();
--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -20,63 +20,17 @@ other variables within the application that may share the same name or need to b
 	angular
 	.module('profileApp', ['ngResource'])
 	.controller('profileController', 
-		['$scope', '$resource', function($scope, $resource){
-		
-		//This new $resource object allows us to query this API, 
-		//and will return the results to a field in the browser.
-		var Profile = $resource("/api/:id", {id: "@id"},
-			{enter : {
-                                      method: "GET", 
-                                      isArray: false
-                                     }
-                            });
+		['$scope', '$http', function($scope, $http){
 
-		//bind the getClicks method to $scope
-		$scope.getProfile = function(){
-			/*
-			 This can then be either manipulated in some way before passing it on to 
-			 he browser, or (as in our case) just pass it straight in to a variable on 
-			 the $scope. 
-			*/
-
-			//Click.get() will make an HTTP GET request to the API and return all of the results.
-			Profile.get(function(results){
-				//$scope.clicks = results.clicks;
-				//console.log(results);
+			$http.get("/api/:id").then(function(response){
+				//console.log("response:" + JSON.stringify(response));
+				var results = response.data;
 				$scope.profileId = results.id;
 				$scope.profileUsername = results.username;
 				$scope.displayName = results.displayName;
 				$scope.profileRepos = results.publicRepos;
-
-
 			});
-		}
 
-		$scope.getProfile();
-
-		/*
-		For addClick, we're instructing Angular to use $resource.save, which will 
-		prompt an HTTP POST request. This in turn will get routed by our index.js 
-		file and run the clickHandler.addClick method on the database. 
-		Once that action has been performed, we query the API via $scope.getClicks, 
-		therefore forcing the $scope.clicks variable to update and represent 
-		the new number of clicks.
-		*/
-/*
-		//prompt an HTTP POST request
-		$scope.addClick = function(){
-			Click.save(function(){
-				$scope.getClicks();
-			});
-		};
-		
-		//HTTP DELETE request
-		$scope.resetClicks = function(){
-			Click.remove(function(){
-				$scope.getClicks();
-			});
-		};
-*/
 	}]);
 
 })();
